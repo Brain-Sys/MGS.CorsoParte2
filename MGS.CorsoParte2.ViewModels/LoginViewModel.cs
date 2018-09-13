9c173dab948ea56dcf30ed36012d046d36723104
Damiani Liborio Igor <igor.damiani@hotmail.com>
2018-09-13 11:20:10 +0200

    Add project files.

---
+++ MGS.CorsoParte2.ViewModels/LoginViewModel.cs
@@ -0,0 +1,83 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MGS.CorsoParte2.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private string username;
+        public string Username
+        {
+            get { return username; }
+            set { username = value;
+                base.RaisePropertyChanged();
+            }
+        }
+
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set { password = value;
+                base.RaisePropertyChanged();
+            }
+        }
+
+        private bool remember;
+        public bool Remember
+        {
+            get { return remember; }
+            set { remember = value;
+                base.RaisePropertyChanged();
+            }
+        }
+
+        private string message;
+        public string Message
+        {
+            get { return message; }
+            set { message = value;
+                base.RaisePropertyChanged();
+            }
+        }
+
+        public RelayCommand LoginCommand { get; set; }
+
+        public LoginViewModel()
+        {
+            // this.Message = "g ndf,mgndfkjgndkjgnsdkjngkfdngkjdfngkjdngfkjndfgkj";
+            this.Username = "emmegisoft";
+            this.Password = "carpi";
+
+            this.LoginCommand = new RelayCommand
+                (loginCommandExecute, loginCommandCanExecute);
+        }
+
+        private bool loginCommandCanExecute()
+        {
+            if (string.IsNullOrEmpty(this.Username) ||
+               string.IsNullOrEmpty(this.Password))
+                return false;
+
+            return true;
+        }
+
+        private void loginCommandExecute()
+        {
+            if (this.Username != "emmegisoft" && this.Password != "carpi")
+            {
+                // Login fallito
+                this.Message = "Login fallito!";
+            }
+            else
+            {
+                
+            }
+        }
+    }
+}
---
+++ MGS.CorsoParte2.WindowsApp/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MGS.CorsoParte2.WindowsApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            string[] parametri = e.Args;
+            LoginWindow wnd = new LoginWindow();
+            wnd.ShowDialog();
+        }
+    }
+}
